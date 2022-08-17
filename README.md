5ff60a0dd28957ad5cee8113e501a809f48b0ce5
Astrid <jmdg1023@gmail.com>
2022-08-17 14:30:57 +1000

    readme updated

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # Changeling003
+
+Changeling - A word of play for 'Challenging'
+
+003 - Natural number following 002 (The previous challenge in this course) 
+
+
+## What this Generate Password site does 💡💡💡
+
+1. When GENERATE PASSWORD button is clicked a promt will show asking to Input password length required between 8 to 128. Entering anything other than an Integer entering numbers lower than 8 or higher than 128 will alert the user "Please enter a valid Number. Length must be between 8 to 128." 
+
+
+2. After Entering an acceptable password length, user will be presented with a series of prompts for password criteria below
+
+* Do you want to include lowercase in your password?
+* Do you want to include uppercase in your password?
+* Do you want to include numbers in your password?
+* Do you want to include symbols characters?
+
+Depending on users answer (true or false) will determine which criteria will be used in generating a password.
+
+
+3. When all of the criteria returns false, the password won't generate and instead return an alert saying "At least one character type should be selected. Please try again."
+
+4. When all prompts are answered, a password is generated that matches the selected criteria (length, lowercase, uppercase, numbers, symbols).
+
+7.Generated password will show up in the 'Secure password' Box.
+
+ ##  Links 📌
+
+* GitHub Repository link https://github.com/jmdg1023/Changeling003
+
+* Site is live at https://jmdg1023.github.io/Changeling003/
+
+ 
+
+
+
+
+
+
+
+
+
--- script.js
+++ script.js
@@ -1,144 +1,80 @@
 // Assignment code here
-
-function promptMe(){
-   
-  var Length = parseInt(window.prompt("Please enter length of password between 8-128:"));
-
-  if (isNaN(Length)){
-    window.alert("Please enter a valid Number between 8 to 128.")
-    return
-  }
-
-  else if (Length < 8 || Length > 128){
-    window.alert("Password length must be between 8 to 128.")
-    return
-  }
-  var choiceArr = []
-  //var Lower = String(window.confirm("Do you want to include lowercase?"));
-  // var Upper = String(window.confirm("Do you want to include uppercase?"));
-  // var Number = parseInt(window.confirm("Do you want to include numeric characters?"));
-  // var Symbol = String(window.confirm("Do you want to include special characters?"));
-
-  if (confirm("Do you want to include lowercase in your password?")){
-    choiceArr = choiceArr.concat(getRandomLower());
-    console.log(choiceArr);
-    return
-  }
-  if (confirm("Do you want to include upperrcase in your password?")){
-    choiceArr = choiceArr.concat(getRandomUpper());
-    return
-  }
-  if (confirm("Do you want to include numbers in your password?")){
-    choiceArr = choiceArr.concat(getRandomNumber());
-    return
-  }
-  if (confirm("Do you want to include symbols in your password?")){
-    choiceArr = choiceArr.concat(getRandomSymbol());
-    return
-  }
-
-
- 
- 
-  if(Lower === false && Number === false && Upper === false && Symbol === false){
-    console.log("Atleast one character type should be selected. Please try again. ")    
-    return
-
-  }
-}
-
+var inputLength = 8;
+var choiceArr = [];
+var lowerArr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var upperArr = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var numbersArr =  ['0','1','2','3','4','5','6','7','8','9'];
+var symbolsArr = ['!','@','#','$','%','^','&','*','(',')','-','+','=','<','>','?','/'];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-//Generate event listener---issue
-// generateEl.addEventListener('click', () =>{
-//   const length = + lengthEl.value;
-//   const hasLower = lowercaseEl.true;
-//   const hasUpper = uppercaseEl.confirm;
-//   const hasNumber = numbercaseEl.confirm;
-//   const hasSymbol = symbolcaseEl.confirm;
 
+// Write password to the #password input
+function writePassword() {
+  var confirmPrompt = promptMe(); //T or  F
+  var passwordText = document.querySelector("#password");
   
-// resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length);
-
-// });
-
-
-
-
-//DOM elements
-const resultEl = document.getElementById('result');
-const lengthEl = document.getElementById('length');
-const numbersEl = document.getElementById('numbers');
-const uppercaseEl = document.getElementById('uppercase');
-const lowercaseEl = document.getElementById('lowercase');
-const symbolsEl = document.getElementById('symbols');
-const generateEl = document.getElementById('generate');
-const clipboardEl = document.getElementById('clipboard');
-
-
-const randomFunc = {
-  lower: getRandomLower,
-  upper: getRandomUpper,
-  number: getRandomNumber,
-  symbol: getRandomSymbol
-
-};
+  if(confirmPrompt){
+    var password = generatePassword();
+    passwordText.value = password;
+  } else{
+    passwordText.value = "";
+  }
+}
 
 //generat password function
-function generatePassword(lower, upper, number, symbol, length){
+function generatePassword(){
 
-  let generatePassword = '';
-  const typesCount = lower + upper + number + symbol;
+  var generatePassword = '';
+
+  for(var i=0; i < inputLength; i++){
+    var randomGP = Math.floor(Math.random() * inputLength);
+    generatePassword = generatePassword + choiceArr[randomGP];
+  }
+  return generatePassword;
+}
+
+function promptMe(){
+  choiceArr = [];
+  inputLength = parseInt(prompt("Please enter length of password between 8-128:"));
+
   
-  const typesArr = [{lower}, {upper}, {number}, {symbol}].filter
-  (item => Object.values(item)[0]);
-
-  if(typesCount === 0){
-    return '';
+  if (isNaN(inputLength) || inputLength < 8 || inputLength > 128){
+    window.alert("Please enter a valid Number. Length must be between 8 to 128.");
+   return false;
   }
+  // var Lower = String(window.confirm("Do you want to include lowercase in your password?"));
+  // var Upper = String(window.confirm("Do you want to include uppercase in your password?"));
+  // var Number = parseInt(window.confirm("Do you want to include numbers in your password?"));
+  // var Symbol = String(window.confirm("Do you want to include symbols characters?"));
 
-  for(let i=0; i< length; i += typesCount){
-    typesArr.forEach(type =>{
-      const funcName = Object.keys(type)[0];
-
-      generatePassword  += randomFunc[funcName]();
-    });
+  if (confirm("Do you want to include lowercase in your password?")){
+    choiceArr = choiceArr.concat(lowerArr);
   }
-  const finalPassword = generatePassword.slice(0, length);
-  return finalPassword;
+  if (confirm("Do you want to include uppercase in your password?")){
+    choiceArr = choiceArr.concat(upperArr);
+  
+  }
+  if (confirm("Do you want to include numbers in your password?")){
+    choiceArr = choiceArr.concat(numbersArr);
+  
+  }
+  if (confirm("Do you want to include symbols characters?")){
+    choiceArr = choiceArr.concat(symbolsArr);
+  }
+  return true;
+
+  // if(Lower === false && Number === false && Upper === false && Symbol === false){
+  //   console.log("Atleast one character type should be selected. Please try again. ")    
+  //   return
+
+  // }
+ 
 }
 
 
 
-
-//Random functions
-
-function getRandomLower(){
-  return String.fromCharCode(Math.floor(Math.random() * 26)  + 97);
-}
-
-function getRandomUpper(){
-  return String.fromCharCode(Math.floor(Math.random() * 26)  + 65);
-}
-function getRandomNumber(){
-  return String.fromCharCode(Math.floor(Math.random() * 10)  + 48);
-}
-
-function getRandomSymbol(){
-  const symbol = ''
-  return String[Math.floor(Math.random() * symbols.length)];
-}
