c30eadb2c1200227be1a417d8ea8e31b3c6c9aae
Astrid <jmdg1023@gmail.com>
2022-08-17 18:54:39 +1000

    generate password working

--- script.js
+++ script.js
@@ -6,6 +6,36 @@ var upperArr = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P',
 var numbersArr =  ['0','1','2','3','4','5','6','7','8','9'];
 var symbolsArr = ['!','@','#','$','%','^','&','*','(',')','-','+','=','<','>','?','/'];
 
+
+function promptMe(){
+  choiceArr=[];
+  inputLength = parseInt(prompt("Please enter length of password between 8-128:"));
+  
+
+  if (isNaN(inputLength) || inputLength < 8 || inputLength > 128){
+    alert("Please enter a valid Number. Length must be between 8 to 128.");
+   return false;
+  }
+
+  if (confirm("Do you want to include lowercase in your password?")){
+    choiceArr = choiceArr.concat(lowerArr);
+  }
+  
+  if (confirm("Do you want to include uppercase in your password?")){
+    choiceArr = choiceArr.concat(upperArr);
+  }
+
+  if (confirm("Do you want to include numbers in your password?")){
+    choiceArr = choiceArr.concat(numbersArr);
+  }
+
+  if (confirm("Do you want to include symbols characters?")){
+    choiceArr = choiceArr.concat(symbolsArr);
+  }
+
+  return true;
+};
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -17,64 +47,28 @@ generateBtn.addEventListener("click", writePassword);
 function writePassword() {
   var confirmPrompt = promptMe(); //T or  F
   var passwordText = document.querySelector("#password");
-  
+
   if(confirmPrompt){
-    var password = generatePassword();
-    passwordText.value = password;
+    var newPassword = generatePassword();
+    passwordText.value = newPassword;
   } else{
     passwordText.value = "";
   }
 }
 
-//generat password function
+//generate password function
 function generatePassword(){
 
-  var generatePassword = '';
+  var password = "";
 
   for(var i=0; i < inputLength; i++){
-    var randomGP = Math.floor(Math.random() * inputLength);
-    generatePassword = generatePassword + choiceArr[randomGP];
+    var randomGP = Math.floor(Math.random() * choiceArr.length);
+    password = password + choiceArr[randomGP];
   }
-  return generatePassword;
+  return password;
 }
 
-function promptMe(){
-  choiceArr = [];
-  inputLength = parseInt(prompt("Please enter length of password between 8-128:"));
 
-  
-  if (isNaN(inputLength) || inputLength < 8 || inputLength > 128){
-    window.alert("Please enter a valid Number. Length must be between 8 to 128.");
-   return false;
-  }
-  // var Lower = String(window.confirm("Do you want to include lowercase in your password?"));
-  // var Upper = String(window.confirm("Do you want to include uppercase in your password?"));
-  // var Number = parseInt(window.confirm("Do you want to include numbers in your password?"));
-  // var Symbol = String(window.confirm("Do you want to include symbols characters?"));
-
-  if (confirm("Do you want to include lowercase in your password?")){
-    choiceArr = choiceArr.concat(lowerArr);
-  }
-  if (confirm("Do you want to include uppercase in your password?")){
-    choiceArr = choiceArr.concat(upperArr);
-  
-  }
-  if (confirm("Do you want to include numbers in your password?")){
-    choiceArr = choiceArr.concat(numbersArr);
-  
-  }
-  if (confirm("Do you want to include symbols characters?")){
-    choiceArr = choiceArr.concat(symbolsArr);
-  }
-  return true;
-
-  // if(Lower === false && Number === false && Upper === false && Symbol === false){
-  //   console.log("Atleast one character type should be selected. Please try again. ")    
-  //   return
-
-  // }
- 
-}
 
 
 
